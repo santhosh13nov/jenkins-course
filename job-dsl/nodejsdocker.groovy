a86b8a4999e64e8e1db16d03c9ef65d3ebe8e7a1
santhosh13nov <30766879+santhosh13nov@users.noreply.github.com>
2018-10-07 11:51:17 +0100

    Update nodejsdocker.groovy

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -16,7 +16,7 @@ job('NodeJS Docker example') {
         dockerBuildAndPublish {
             repositoryName('santhosh13nov/docker-nodejs-demo')
             tag('${GIT_REVISION,length=9}')
-            registryCredentials('dockerhub')
+            registryCredentials('dockerhub_cred')
             forcePull(false)
             forceTag(false)
             createFingerprints(false)
